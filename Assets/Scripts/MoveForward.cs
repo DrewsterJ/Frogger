73d9918cf947f24e871647f56c0a2a9dfed7c2b7
DrewsterJ <drewpjonester@gmail.com>
2023-09-03 19:21:30 -0400

    Updated SpawnManager and MoveForward scripts to support spawning entities and moving entities to the left and right upon being spawned

---
+++ Assets/Scripts/MoveForward.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    private float speed = 5.0f;
+    public bool leftFacing = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (leftFacing)
+        {
+            transform.Translate(Vector2.left * (Time.deltaTime * speed));
+        }
+        else
+        {
+            transform.Translate(Vector2.right * (Time.deltaTime * speed));
+        }
+        
+    }
+}
