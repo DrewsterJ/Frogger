f5fa740c75ab28e318d6e51d801ceb34a0073a4b
DrewsterJ <drewpjonester@gmail.com>
2023-09-10 11:36:14 -0400

    Implemented player ability to win after scoring 3 points

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,11 +20,14 @@ public class PlayerController : MonoBehaviour
 
     public GameObject camera;
     private AudioControl audioControlScript;
+    public GameObject gameManager;
+    private GameManager gameManagerScript;
 
     private void Start()
     {
         uiScript = uiOverlay.GetComponent<UI>();
         audioControlScript = camera.GetComponent<AudioControl>();
+        gameManagerScript = gameManager.GetComponent<GameManager>();
     }
     
     void Update()
@@ -104,10 +107,15 @@ public class PlayerController : MonoBehaviour
 
         if (uiScript.score == 3)
         {
+            gameManagerScript.WinGame();
             audioControlScript.wonMusic.Play();
+            audioControlScript.scoreAudio.Play();
         }
-        audioControlScript.scoreAudio.Play();
-        transform.position = startLocation;
+        else
+        {
+            audioControlScript.scoreAudio.Play();
+            transform.position = startLocation;
+        }
     }
 
     private void HandleFloatingObjectCollision(Collider floatingObject)
